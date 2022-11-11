849b1a0c4ba702f573c507ae97ad8a7345475296
Ryan-Otoole <otoolerj@gmail.com>
2022-11-10 18:32:17 -0600

    pushing up barebone project for weekend

---
+++ contracts/TokenizedBallot.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/// @title Voting with delegation.
+
+interface IMyToken {
+    function getPastVotes(address, uint256) external view returns (uint256);
+}
+
+contract TokenizedBallot {
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
+
+    IMyToken public tokenContract;
+    Proposal[] public proposals;
+    uint256 public targetBlockNumber;
+
+    mapping(address => uint256) public votingPowerSpent;
+
+    constructor(
+        bytes32[] memory _proposalNames,
+        address _TokenContractAddress,
+        uint256 _targetBlockNumber
+    ) {
+        tokenContract = IMyToken(_TokenContractAddress);
+        for (uint i = 0; i < _proposalNames.length; i++) {
+            proposals.push(Proposal({name: _proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    /// Give your vote (including votes delegated to you)
+    /// to proposal `proposals[proposal].name`.
+    function vote(uint256 proposal, uint256 amount) external {
+        require(votingPower(msg.sender) >= amount);
+        // TODO
+        votingPowerSpent[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256) {
+        return tokenContract.getPastVotes(account, targetBlockNumber);
+    }
+
+    /// @dev Computes the winning proposal taking all
+    /// previous votes into account.
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    // Calls winningProposal() function to get the index
+    // of the winner contained in the proposals array and then
+    // returns the name of the winner
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
